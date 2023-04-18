37318f36d0916304b98abf3fc9dcf03ef364d4df
joaopsgomes <92636649+joaoplayfm@users.noreply.github.com>
2023-04-18 22:55:29 +0100

    Delete bddad/Sprints directory

--- bddad/Sprints/US206/bddad_US206.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM Parcelas ORDER BY designacao ASC;
-SELECT * FROM Parcelas ORDER BY area DESC;
-SELECT * FROM Parcelas ORDER BY area ASC;
-SELECT * FROM Parcelas ORDER BY cultura ASC;
-SELECT * FROM Parcelas ORDER BY tipo_cultura ASC;
